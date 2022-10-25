625783512c240fca7c78b3175c5dcee24aef03d8
Miguel Henriques <miguel.c.henriques22@gmail.com>
2022-10-25 18:47:23 +0100

    Added adder endpoints for operations

--- project/adder/adder/main.py
+++ project/adder/adder/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get('/health')
 async def health_check():
     return {"status": "OK"}
+
+@app.get('/sum')
+async def sum(a: int, b: int) -> int:
+    return a + b
+
+@app.get('/subtraction')
+async def subtraction(a: int, b: int) -> int:
+    return a - b
