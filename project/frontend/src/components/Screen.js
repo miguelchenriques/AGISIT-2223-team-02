4170162a9af98790636037a6f4613666e481e662
ist1102150 <bernardo.c.varzea@tecnico.ulisboa.pt>
2022-11-01 10:47:13 +0000

    add frontend

---
+++ project/frontend/src/components/Screen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DisplayScreen = (props) => {
+    const { total, next, operation } = props;
+    return (
+      <div className="displayscreen-cont">
+        <span className="display-screen">
+        {total}
+          {operation}
+          {next}
+           </span>
+      </div>
+    );
+  }
+
+export default DisplayScreen;
