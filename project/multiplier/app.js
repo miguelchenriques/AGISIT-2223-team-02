71baee0af8d48b38a38fa5ee853d742c095d445b
FilipeABBarroso <filipebarroso2812@gmail.com>
2022-11-02 18:38:31 +0000

    Add insertInDB function in multiplier

--- project/multiplier/app.js
+++ project/multiplier/app.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const createError = require('http-errors');
 
 const multiplier = require('./routes/multiplier');
 
 const app = express();
 
 app.use(multiplier);
-
-
-app.use((req, res, next) => {
-    next(createError.NotFound());
-    next();
-});
 
 module.exports = { app };
